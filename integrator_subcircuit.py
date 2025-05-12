816ea2f1849983d62ac0a8acbe643b5100804f78
snietofennis <165265559+snietofennis@users.noreply.github.com>
2025-05-12 10:46:51 +0200

    Add files via upload

--- integrator_subcircuit.py
+++ integrator_subcircuit.py
@@ -8,17 +8,16 @@ from PySpice.Unit import *
 
 # measured has to be a component so that we can measure the current through it
 # i_out is the integrated current output, but is given as a voltage
-circuit.B('1', 'measured', circuit.gnd, voltage_expression='I(component)')
+circuit.B('1', 'name_measured', circuit.gnd, voltage_expression='I(component)')
 
 
-
-        self.raw_spice += f"""
-A2 measured i_out i_model
+circuit.raw_spice += f"""
+A2 mname_measured idt_name i_model
 .model i_model int(gain=1 in_offset=0 
 + out_lower_limit=-1e12 out_upper_limit=1e3
 + limit_range=1e-9 out_ic=0)
 R3 i_out 0 1Meg
 """
         
-circuit.B('2', circuit.gnd, circuit.gnd, current_expression='V(i_out)')
+circuit.B('2', circuit.gnd, circuit.gnd, current_expression='V(idt_name) / R3')
 
